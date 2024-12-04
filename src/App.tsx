1e86d6931813261f18ffbc452d66852e550fa5e9
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-12-04 22:40:02 +0200

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import MealDetails from "./components/MealDetails/MealDetails.tsx";
 import SelectedMeals from "./components/SelectedMeals/SelectedMeals.tsx";
 
 function App() {
-
-    console.log(import.meta.env);
   return (
       <BrowserRouter>
           <div className="flex flex-col min-h-screen justify-between">
